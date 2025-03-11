a4300834ecf9380cb279bbb56694baaee0115d2c
StaticCloud <diegomarrs@gmail.com>
2025-03-10 20:44:23 -0700

    added Spectre.Console to dependencies

---
+++ src/Program.cs
@@ -0,0 +1,13 @@
+﻿using Spectre.Console;
+
+namespace Cargo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            AnsiConsole.MarkupLine("[underline][white]Welcome to[/] [bold blue]Cargo![/][/]");
+            AnsiConsole.MarkupLine("[white bold]This is still a work in progress. Please visit the [blue][link=https://github.com/StaticCloud/Cargo]GitHub[/][/] page for updates![/]");
+        }
+    }
+}
