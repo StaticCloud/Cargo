186b7540f41cb786b16e22e05a4d4e5a0d160cbc
StaticCloud <diegomarrs@gmail.com>
2025-03-16 09:54:17 -0700

    added loading screen for Docker engine connection

--- src/api/Connection.cs
+++ src/api/Connection.cs
@@ -1,4 +1,5 @@
 ﻿using Docker.DotNet;
+using Spectre.Console;
 
 namespace Cargo.src.api
 {
@@ -10,19 +11,25 @@ namespace Cargo.src.api
         {
             _client = new DockerClientConfiguration().CreateClient();
 
-            try
-            {
+            AnsiConsole.Status().Start("[blue]Connecting to Docker engine...[/]", ctx => {
+                ctx.Spinner(Spinner.Known.Star);
+                ctx.SpinnerStyle(Style.Parse("blue"));
+                
+                try
+                {
+                    AnsiConsole.Clear();
 
-                Task ping = _client.System.PingAsync();
+                    Task ping = _client.System.PingAsync();
 
-                ping.Wait();
+                    ping.Wait();
 
-                Console.WriteLine(ping.Status);
-            }
-            catch (Exception ex) 
-            {
-                Console.WriteLine(ex.ToString());
-            }
+                    Console.WriteLine(ping.Status);
+                }
+                catch
+                {
+                    AnsiConsole.MarkupLine("[red]Connection to the Docker engine could not be established. Please make sure it is running locally on your machine.[/]");
+                }
+            });
         }
     }
 }
